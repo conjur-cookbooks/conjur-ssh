b963a7fc1bbdba2863a34d02ef595dec7d33218b
Hleb Rubanau <g.rubanau@gmail.com>
2014-08-21 19:44:16 +0300

    add logshipper for ubuntu precise

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,6 @@ include_recipe "conjur-client::ssl_certificate" if node.conjur['ssl_certificate'
 include_recipe "terminal-login::install" 
 include_recipe "terminal-login::sudoers" if node['conjur-ssh'].install_sudoers
 include_recipe "terminal-login::configure" if node['conjur-ssh'].configure
+if ('ubuntu' == node['platform'] and Chef::VersionConstraint.new('<= 12.04').include?(node['platform_version'])) 
+  include_recipe "terminal-login::logshipper"
+end
