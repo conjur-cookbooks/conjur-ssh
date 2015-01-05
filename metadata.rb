5736581982d75cc8f3f18886d38b9bf4195568ee
Dustin Collins <dustinrcollins@gmail.com>
2015-01-05 12:05:33 -0500

    [#85362492] v1.2.4

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Conjur, Inc.'
 maintainer_email  'kgilpin@conjur.net'
 license           'Apache 2.0'
 description       'Installs Conjur SSH'
-version           '0.1.0'
+version           '1.2.4'
 
 attribute 'conjur-ssh/install_client',
   description: "Whether to install the Conjur CLI",
